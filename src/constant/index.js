038c3780b9fc54b884afdf69087726990241204f
Iris Huang <lyaui@outloook.com>
2021-04-27 15:20:01 +0800

    set menu items constant

---
+++ src/constant/index.js
@@ -0,0 +1,11 @@
+export const MENU_ITEMS = {
+  ADD_TASK: 'ADD NEW TASK',
+  TASK_LISTS: 'TASK LISTS',
+  ANALYTICS_REPORT: 'ANALYTICS REPORT',
+  RING_TONE: 'RING TONE',
+};
+
+export const INITIAL_TIME_LENGTH = {
+  WORK: 25 * 60,
+  break: 5 * 60,
+};
