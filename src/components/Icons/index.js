cb974ae629d3e6de85e7066217766beefde2a99a
Iris Huang <lyaui@outloook.com>
2021-04-27 15:18:55 +0800

    set icons components

---
+++ src/components/Icons/index.js
@@ -0,0 +1,20 @@
+// menu icons
+import { ReactComponent as AddTask } from '../../assets/icons/add_white.svg';
+import { ReactComponent as AddTaskActive } from '../../assets/icons/add_red.svg';
+import { ReactComponent as List } from '../../assets/icons/list_white.svg';
+import { ReactComponent as ListActive } from '../../assets/icons/list_red.svg';
+import { ReactComponent as Analysis } from '../../assets/icons/analysis_white.svg';
+import { ReactComponent as AnalysisActive } from '../../assets/icons/analysis_red.svg';
+import { ReactComponent as RingTone } from '../../assets/icons/ringtone_white.svg';
+import { ReactComponent as RingToneActive } from '../../assets/icons/ringtone_red.svg';
+
+export {
+  AddTask,
+  AddTaskActive,
+  List,
+  ListActive,
+  Analysis,
+  AnalysisActive,
+  RingTone,
+  RingToneActive,
+};
