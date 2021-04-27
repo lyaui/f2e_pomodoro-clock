43656186ec1d6d2de2c41d549111fd1118da5227
Iris Huang <lyaui@outloook.com>
2021-04-27 15:21:01 +0800

    set panel components

---
+++ src/components/MenuPanels/ManageTodos/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ManageTodos = () => {
+  return <div>ManageTodos</div>;
+};
+
+export default ManageTodos;
---
+++ src/components/MenuPanels/StatisticReport/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ManageSounds = () => {
+  return <div>ManageSounds</div>;
+};
+
+export default ManageSounds;
